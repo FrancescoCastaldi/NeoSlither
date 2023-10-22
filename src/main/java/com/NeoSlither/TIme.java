167c53b47f19b789cf4f97f46d51048f8c1ac194
Francesco.Castaldi <290354@studenti.unimore.it>
2023-10-22 20:05:18 +0200

    Refixed all

--- src/main/java/com/NeoSlither/TIme.java
+++
@@ -1,7 +0,0 @@
-package com.NeoSlither;
-
-public class TIme {
-    public static double timeStored = System.nanoTime();
-    public static double getTime(
-            return (System).nanoTime() - timeStored ) * 1E-9;
-}
